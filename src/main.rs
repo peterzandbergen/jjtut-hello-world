a455e73a1b7aa70cd50444d9497b740825f884a9
Peter Zandbergen <peter.zandbergen@myhops.com>
2025-05-05 16:28:38 +0200

    remove goodbye message

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@
 /// This is the best implementation of this program to ever exist.
 fn main() {
     print("Hello, world!");
-    print("Goodbye, world!");
 }
 
 // a function that prints a message
 fn print(m: &str) {
     println!("{m}");
 }
-    
