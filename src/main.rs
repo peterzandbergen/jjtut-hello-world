1d22bd5738c9b9d7f0a538557107fff34dc36693
Peter Zandbergen <peter.zandbergen@myhops.com>
2025-05-06 08:32:48 +0200

    add new function

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 ///
 /// This is the best implementation of this program to ever exist.
 
-/// add documentation for main
+/// add documentation for main 
 fn main() {
     print("Hello, world!");
     print("Goodbye, world!");
